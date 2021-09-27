709be5aa5980eb7ce3748710875dc9aaf8cd155f
DanilHidayat409 <91053707+DanilHidayat409@users.noreply.github.com>
2021-09-27 15:17:54 +0700

    Add files via upload

---
+++ operasi aritmatika-Muhammad Danil Hidayat.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 14:32:50 2021
+
+@author: Muhammad Danil Hidayat
+@nim : 065.002.100.032
+"""
+
+import math
+
+angka1 = float(input("Masukan nilai a = "))
+angka2 = float(input("Masukan nilai b = "))
+
+hasil1 = angka1 + angka2
+print("jumlah a ditambah b adalah", hasil1)
+
+hasil2 = angka1 - angka2
+print("selisih antara a dan b adalah", hasil2)
+
+hasil3 = angka1 * angka2
+print("hasil kali a dan b adalah", hasil3)
+
+hasil4 = angka1 % angka2
+print("jumlah sisa pembagian dari hasil a dibagi b adalah", hasil4)
+
+hasil5 = angka1 / angka2
+print("jumlah a dibagi b adalah", hasil5)
+
+hasil8 = math.log10(angka1)
+print("hasil log(a)adalah", hasil8)
+
+hasil6 = angka1 ** angka2
+print("hasil a pangkat b adalah", hasil6)
